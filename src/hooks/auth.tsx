311e9a3c4439b530e018bd16c00dceb994f6f62e
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2020-12-29 04:28:44 -0300

    finding providers api

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable camelcase */
-/* eslint-disable @typescript-eslint/ban-types */
 import React, {
   createContext,
   useCallback,
@@ -48,6 +46,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -67,6 +66,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,16 +11,30 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
   const { signOut, user } = useAuth();
   const { navigate } = useNavigation();
 
-  const navigateToProfile = useCallback(() => {
-    navigate('Profile');
-  }, [navigate]);
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
+  const navigateToProfile = useCallback(() => {
+    // navigate('Profile');
+    signOut();
+  }, [navigate]);
   return (
     <Container>
       <Header>
@@ -33,6 +48,12 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+        keyExtractor={provider => provider.id}
+      />
     </Container>
   );
 };
